54980aaf7bd67c023ad22fcd5c2971f086870691
androrayhan21 <56229674+androrayhan21@users.noreply.github.com>
2019-10-14 10:59:43 +0700

    Add files via upload

---
+++ UTS pemrograman dasar/UTS.cpp
@@ -0,0 +1,12 @@
+
+ #include <iostream>
+ using namespace std; 
+  int main()
+  {
+  int i=99, a;
+  cout<<"Masukkan nim : ";cin>>a;
+  awal: a++;
+  cout<<a<<","; if(i>=a)
+  goto awal; else
+  goto akhir; akhir: return 0;
+  }
---
+++ UTS pemrograman dasar/UTS1.cpp
@@ -0,0 +1,12 @@
+
+ #include <iostream>
+ using namespace std;
+ int main ()
+ {
+ int umur; cout<<"Masukkan umur:";
+ cin>>umur; 
+ if(umur<17)
+ cout<<"Belum meiliki KTP";
+
+ return 0;
+ }
